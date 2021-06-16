137d977560d3a7628215354133d60825060038f0
yuto-takami <orca.y1322@gmail.com>
2021-06-16 13:50:05 +0000

    [totyuu]kadai

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'top' => 'homes#top'
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show', as: 'book'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
